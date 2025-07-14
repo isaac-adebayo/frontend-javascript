3f6df33bcad0cc00dd28368359d14c24b8fd595e
isaac-adebayo <isaacadebayo.ai@gmail.com>
2025-07-14 20:03:20 +0100

    Writing a Class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,41 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 
 // Example usage
 console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+// Writing a class
+
+// Interface describing the constructor arguments
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Interface describing the class shape
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Implementing the class using the interfaces
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student = new StudentClass({ firstName: "Isaac", lastName: "Adebayo" });
+console.log(student.displayName());     // Output: Isaac
+console.log(student.workOnHomework());  // Output: Currently working
